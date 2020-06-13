c472e3004cb1054b53a8b593fdd012c704a9744a
Yevhen <yevhen.code@gmail.com>
2020-06-13 13:16:14 +0300

    add simple slaider

--- js/script.js
+++ js/script.js
@@ -285,4 +285,54 @@ document.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
+
+    // simple Slider
+
+    const slides = document.querySelectorAll('.offer__slide'),
+          prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          total = document.querySelector('#total'),
+          current = document.querySelector('#current');
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        slides[slideIndex - 1].style.display = 'block';
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    }
+
+    function plusSlide(n) {
+        showSlides(slideIndex += n);
+    }
+    
+    prev.addEventListener('click', () => {
+        plusSlide(-1);
+    });
+
+    next.addEventListener('click', () => {
+        plusSlide(1);
+    });
 });
